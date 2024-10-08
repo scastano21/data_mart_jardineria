d5f7af4e9c205abfc381e0fc6f00e8525243aca5
scastano21 <49565873+scastano21@users.noreply.github.com>
2024-10-08 15:43:59 -0500

    Delete calidad_datos.sql

--- calidad_datos.sql
+++
@@ -1,9 +0,0 @@
-# Pruebas de Calidad de Datos
-
-## 1. Prueba de duplicados en la tabla de Clientes
-**Consulta**: Se utilizó la siguiente consulta para identificar y eliminar duplicados en la tabla de clientes:
-```sql
-SELECT ID_cliente, COUNT(*) 
-FROM Stg_Cliente 
-GROUP BY ID_cliente 
-HAVING COUNT(*) > 1;
